eef5d8ff4135e2f288a480a9e897865b7b942adc
Bobby Rae <r.rae.dev@gmail.com>
2020-08-29 22:08:04 -0400

    remove media plugin, add fields to track post type

--- wp-content/themes/harp-space/lib/carbon.php
+++ wp-content/themes/harp-space/lib/carbon.php
@@ -4,10 +4,12 @@ use Carbon_Fields\Field;
 
 add_action( 'carbon_fields_register_fields', 'attach_theme_options' );
 add_action( 'carbon_fields_register_fields', 'attach_page_fields' );
+add_action( 'carbon_fields_register_fields', 'attach_track_fields' );
 
 function attach_theme_options() {
     Container::make( 'theme_options', 'Theme Options' )
-        ->add_tab( 'Header/Footer', array(
+        ->add_tab( 'Site Info', array(
+            Field::make( 'text', 'album_title', 'Album Title' ),
             Field::make( 'image', 'logo', 'Logo' ),
             Field::make( 'textarea', 'copyright', 'Copyright' ),
             Field::make( 'text', 'signup_shortcode', 'Signup Shortcode' ),
@@ -29,7 +31,6 @@ function attach_theme_options() {
 }
 
 function attach_page_fields() {
-
     Container::make( 'post_meta', 'Hero' )
         ->where( 'post_type', '=', 'page' )
         ->set_context( 'carbon_fields_after_title' )
@@ -65,6 +66,41 @@ function attach_page_fields() {
         ->add_fields( array(
             Field::make( 'text', 'contact_title', 'Contact Title' ),
             Field::make( 'text', 'contact_shortcode', 'Contact Shortcode' )
+        ));
+}
+
+function attach_track_fields() {
+    Container::make( 'post_meta', 'Track Information' )
+        ->where( 'post_type', '=', 'track' )
+        ->set_context( 'carbon_fields_after_title' )
+        ->add_fields( array(
+            Field::make( 'text', 'track_artist', 'Track Artist' ),
+            Field::make( 'text', 'track_duration', 'Track Duration' ),
+            Field::make( 'text', 'track_number', 'Track Number' ),
+            Field::make( 'complex', 'track_credits', 'Track Credits' )
+                ->add_fields( array(
+                    Field::make( 'text', 'artist_name', 'Artist Name' ),
+                    Field::make( 'text', 'artist_instrument', 'Artist Instrument' )
+                )),
+            Field::make( 'select', 'add_preview', 'Add Preview?' )
+                ->add_options( array(
+                    'no' => 'No',
+                    'yes' => 'Yes'
+                )),
+            Field::make( 'file', 'the_track', 'MP3 File' )
+                ->set_type('audio')
+                ->set_conditional_logic( array(
+                    'field' => 'add_preview',
+                    'value' => 'yes'
+                ))
+                ->set_conditional_logic( array(
+                    'relation' => 'AND', // Optional, defaults to "AND"
+                    array(
+                        'field' => 'add_preview',
+                        'value' => 'yes', // Optional, defaults to "". Should be an array if "IN" or "NOT IN" operators are used.
+                        'compare' => '=', // Optional, defaults to "=". Available operators: =, <, >, <=, >=, IN, NOT IN
+                    )
+                ) ),
         ));
 }
 
