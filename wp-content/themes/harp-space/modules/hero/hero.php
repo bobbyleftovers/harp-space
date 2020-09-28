baeb7a4437d60befa67d41c0caecf582d16a5c48
Bobby Rae <r.rae.dev@gmail.com>
2020-09-28 11:14:57 -0400

    hero changes

--- wp-content/themes/harp-space/modules/hero/hero.php
+++ wp-content/themes/harp-space/modules/hero/hero.php
@@ -2,7 +2,7 @@
 $bk_image = wp_get_attachment_image_src( carbon_get_theme_option( 'background_image' ), 'full' )[0];
 $listen_links = '<div class="hero__link-set">';
 foreach($link_set as $link){
-  $listen_links .= '<a href="/" class="link-set__link">';
+  $listen_links .= '<a href="/" class="link-set__link" target="_blank">';
   $listen_links .= get_module('image', array(
     'image' => $link['icon'],
     'class' => 'link-set__image',
@@ -29,7 +29,7 @@ $listen_links .= '</div>';
           'class' => 'details__title-img'
         ));?>
         <div class="details__copy"><?= $copy ?></div>
-        <a href="<?= carbon_get_the_post_meta('pdf_download') ?>" class="details__download"><?php
+        <a href="<?= carbon_get_the_post_meta('pdf_download') ?>" target="_blank" class="details__download"><?php
           the_module('image', array(
             'image' => $secondary_image,
             'class' => 'details__liner-notes',
@@ -40,8 +40,8 @@ $listen_links .= '</div>';
         <div class="details__cta"><?php
           $cta = carbon_get_the_post_meta('hero_cta'); ?>
           <h2 class="details-cta__header"><?= carbon_get_the_post_meta('hero_cta_header') ?></h2>
-          <span class="details-cta__subheader"><?= carbon_get_the_post_meta('hero_cta_subheader') ?></span>
           <a href="<?= $cta['url'] ?>" target="_blank" class="button"><?= $cta['anchor'] ?></a>
+          <span class="details-cta__subheader"><?= carbon_get_the_post_meta('hero_cta_subheader') ?></span>
         </div>
     </div>
   </div>
